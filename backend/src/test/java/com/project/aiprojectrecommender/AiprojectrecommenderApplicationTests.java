b9c4332bbc535d82c6f7ac6965420c6cfb703462
Your Name <saisravani2306@gmail.com>
2026-08-05 09:53:35 +0530

    Initial project foundation

---
+++ backend/src/test/java/com/project/aiprojectrecommender/AiprojectrecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.aiprojectrecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiprojectrecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
